02ebabab38fd820eef029cee5163c6fb7fa04f6a
Alexey <alexdtat@gmail.com>
2025-03-18 03:19:06 +0300

    refactor(algorithms): change of vision scope modifiers.

--- pysatl_cpd/core/algorithms/bayesian_online_algorithm.py
+++ pysatl_cpd/core/algorithms/bayesian_online_algorithm.py
@@ -29,18 +29,18 @@ class BayesianOnlineCpd(OnlineCpdAlgorithm):
         detector: IDetector,
         localizer: ILocalizer,
     ) -> None:
-        self.detector = detector
-        self.hazard = hazard
-        self.likelihood = likelihood
-        self.localizer = localizer
-        self.window_size = learning_sample_size
+        self.__detector = detector
+        self.__hazard = hazard
+        self.__likelihood = likelihood
+        self.__localizer = localizer
+        self.__window_size = learning_sample_size
 
-        self.training_data: list[np.float64] = []
-        self.data_history: list[np.float64] = []
-        self.current_time = 0
+        self.__training_data: list[np.float64] = []
+        self.__data_history: list[np.float64] = []
+        self.__current_time = 0
 
-        self.is_training: bool = True
-        self.run_length_probs: np.ndarray = np.array([])
+        self.__is_training: bool = True
+        self.__run_length_probs: np.ndarray = np.array([])
 
         self.__was_change_point = False
         self.__change_point: int | None = None
@@ -50,63 +50,63 @@ class BayesianOnlineCpd(OnlineCpdAlgorithm):
         Clears the state of the algorithm's instance.
         :return:
         """
-        self.training_data = []
-        self.data_history = []
-        self.current_time = 0
+        self.__training_data = []
+        self.__data_history = []
+        self.__current_time = 0
 
-        self.is_training = True
-        self.run_length_probs = np.array([])
+        self.__is_training = True
+        self.__run_length_probs = np.array([])
 
         self.__was_change_point = False
         self.__change_point = None
 
     def __train(self, value: np.float64) -> None:
-        self.training_data.append(value)
-        if len(self.training_data) == self.window_size:
-            self.likelihood.clear()
-            self.detector.clear()
+        self.__training_data.append(value)
+        if len(self.__training_data) == self.__window_size:
+            self.__likelihood.clear()
+            self.__detector.clear()
 
-            self.likelihood.learn(self.training_data)
-            self.is_training = False
-            self.run_length_probs = np.array([1.0])
+            self.__likelihood.learn(self.__training_data)
+            self.__is_training = False
+            self.__run_length_probs = np.array([1.0])
 
     def __bayesian_update(self, value: np.float64):
-        predictive_prob = self.likelihood.predict(value)
-        current_run_lengths = np.arange(len(self.run_length_probs))
-        hazards = self.hazard.hazard(current_run_lengths)
-        growth_probs = self.run_length_probs * (1 - hazards) * predictive_prob
-        reset_prob = np.sum(self.run_length_probs * hazards * predictive_prob)
+        predictive_prob = self.__likelihood.predict(value)
+        current_run_lengths = np.arange(len(self.__run_length_probs))
+        hazards = self.__hazard.hazard(current_run_lengths)
+        growth_probs = self.__run_length_probs * (1 - hazards) * predictive_prob
+        reset_prob = np.sum(self.__run_length_probs * hazards * predictive_prob)
         new_probs = np.append(reset_prob, growth_probs)
         new_probs /= np.sum(new_probs)
-        self.run_length_probs = new_probs
-        self.likelihood.update(value)
+        self.__run_length_probs = new_probs
+        self.__likelihood.update(value)
 
     def __handle_localization(self) -> None:
-        run_length = self.localizer.localize(self.run_length_probs)
-        change_point_location = self.current_time - run_length
-        self.training_data = self.data_history[-run_length:]
-        self.data_history = self.data_history[-run_length:]
+        run_length = self.__localizer.localize(self.__run_length_probs)
+        change_point_location = self.__current_time - run_length
+        self.__training_data = self.__data_history[-run_length:]
+        self.__data_history = self.__data_history[-run_length:]
         self.__change_point = change_point_location
 
-        self.likelihood.clear()
-        self.detector.clear()
-        self.is_training = True
+        self.__likelihood.clear()
+        self.__detector.clear()
+        self.__is_training = True
 
-        if len(self.training_data) >= self.window_size:
-            self.training_data = self.training_data[: self.window_size]
-            self.likelihood.learn(self.training_data)
-            self.is_training = False
-            self.run_length_probs = np.array([1.0])
+        if len(self.__training_data) >= self.__window_size:
+            self.__training_data = self.__training_data[: self.__window_size]
+            self.__likelihood.learn(self.__training_data)
+            self.__is_training = False
+            self.__run_length_probs = np.array([1.0])
 
-            for value in self.data_history[self.window_size + 1 :]:
+            for value in self.__data_history[self.__window_size + 1 :]:
                 self.__bayesian_update(value)
 
     def __handle_detection(self) -> None:
-        self.data_history = self.data_history[-1:]
-        self.training_data = self.data_history[:]
-        self.likelihood.clear()
-        self.detector.clear()
-        self.is_training = True
+        self.__data_history = self.__data_history[-1:]
+        self.__training_data = self.__data_history[:]
+        self.__likelihood.clear()
+        self.__detector.clear()
+        self.__is_training = True
 
     def __process_point(self, value: np.float64, with_localization: bool) -> None:
         """
@@ -115,14 +115,14 @@ class BayesianOnlineCpd(OnlineCpdAlgorithm):
         :param with_localization: whether the method was called for localization of a change point.
         :return:
         """
-        self.data_history.append(value)
-        self.current_time += 1
+        self.__data_history.append(value)
+        self.__current_time += 1
 
-        if self.is_training:
+        if self.__is_training:
             self.__train(value)
         else:
             self.__bayesian_update(value)
-            detected = self.detector.detect(self.run_length_probs)
+            detected = self.__detector.detect(self.__run_length_probs)
 
             if not detected:
                 return
