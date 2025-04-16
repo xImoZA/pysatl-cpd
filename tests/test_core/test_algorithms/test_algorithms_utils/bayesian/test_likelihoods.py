80c6556d9d7587dda7d81280263edfca63d87c85
Alexey <alexdtat@gmail.com>
2025-04-16 23:34:11 +0300

    test(likelihoods): add tests for heuristic likelihood and extended likelihoods

--- tests/test_core/test_algorithms/test_algorithms_utils/bayesian/test_likelihoods.py
+++ tests/test_core/test_algorithms/test_algorithms_utils/bayesian/test_likelihoods.py
@@ -1,8 +1,17 @@
 import numpy as np
 import pytest
 
-from pysatl_cpd.core.algorithms.bayesian.likelihoods.exponential_conjugate import ExponentialConjugate
-from pysatl_cpd.core.algorithms.bayesian.likelihoods.gaussian_conjugate import GaussianConjugate
+from pysatl_cpd.core.algorithms.bayesian.likelihoods.exponential_conjugate import (
+    ExponentialConjugate,
+    ExponentialConjugateWithPriorProbability,
+)
+from pysatl_cpd.core.algorithms.bayesian.likelihoods.gaussian_conjugate import (
+    GaussianConjugate,
+    GaussianConjugateWithPriorProbability,
+)
+from pysatl_cpd.core.algorithms.bayesian.likelihoods.heuristic_gaussian_vs_exponential import (
+    HeuristicGaussianVsExponential,
+)
 
 
 @pytest.fixture(scope="module")
@@ -14,8 +23,9 @@ def set_seed():
     params=[
         (GaussianConjugate, {"pre_loc": 0, "pre_scale": 1, "post_loc": 5, "post_scale": 2}),
         (ExponentialConjugate, {"pre_scale": 1 / 0.5, "post_scale": 1 / 2}),
+        (HeuristicGaussianVsExponential, {"pre_scale": 1 / 0.5, "post_loc": 5, "post_scale": 2}),
     ],
-    ids=["Gaussian", "Exponential"],
+    ids=["Gaussian", "Exponential", "HeuristicGaussianVsExponential"],
 )
 def likelihood_config(request):
     return request.param
@@ -27,20 +37,31 @@ def test_data(likelihood_config, set_seed):
     size = 500
     change_point = 250
 
-    if likelihood_cls == GaussianConjugate:
-        data = np.concatenate(
-            [
-                np.random.normal(params["pre_loc"], params["pre_scale"], change_point),
-                np.random.normal(params["post_loc"], params["post_scale"], size - change_point),
-            ]
-        )
-    else:
-        data = np.concatenate(
-            [
-                np.random.exponential(params["pre_scale"], change_point),
-                np.random.exponential(params["post_scale"], size - change_point),
-            ]
-        )
+    match likelihood_cls():
+        case GaussianConjugate():
+            data = np.concatenate(
+                [
+                    np.random.normal(params["pre_loc"], params["pre_scale"], change_point),
+                    np.random.normal(params["post_loc"], params["post_scale"], size - change_point),
+                ]
+            )
+        case ExponentialConjugate():
+            data = np.concatenate(
+                [
+                    np.random.exponential(params["pre_scale"], change_point),
+                    np.random.exponential(params["post_scale"], size - change_point),
+                ]
+            )
+        case HeuristicGaussianVsExponential():
+            data = np.concatenate(
+                [
+                    np.random.exponential(params["pre_scale"], change_point),
+                    np.random.normal(params["post_loc"], params["post_scale"], size - change_point),
+                ]
+            )
+        case _:
+            raise ValueError("Unsupported likelihood")
+
     return data
 
 
@@ -91,3 +112,53 @@ class TestConjugateLikelihood:
         second = likelihood.predict(np.float64(test_data[-1]))
 
         np.testing.assert_array_equal(first, second)
+
+
+class TestPriorProbabilityOfSample:
+    @pytest.fixture(autouse=True)
+    def setup_teardown(self):
+        np.random.seed(42)
+        self.data_size = 20
+
+    @pytest.fixture(
+        params=[("exponential", "normal"), ("normal", "exponential")],
+        ids=[
+            "exponential data",
+            "normal_data",
+        ],
+    )
+    def test_scenario(self, request):
+        return request.param
+
+    @pytest.fixture
+    def datasets(self):
+        return {
+            "exponential": np.random.exponential(size=self.data_size),
+            "normal": np.random.normal(size=self.data_size),
+        }
+
+    def test_probabilities_of_samples(self, test_scenario, datasets):
+        target_likelihood, compared_likelihood = test_scenario
+        target_data = datasets[target_likelihood]
+
+        target_likelihood = (
+            ExponentialConjugateWithPriorProbability()
+            if target_likelihood == "exponential"
+            else GaussianConjugateWithPriorProbability()
+        )
+        compare_likelihood = (
+            ExponentialConjugateWithPriorProbability()
+            if compared_likelihood == "exponential"
+            else GaussianConjugateWithPriorProbability()
+        )
+
+        target_likelihood.learn(target_data)
+        compare_likelihood.learn(target_data)
+
+        target_prob = target_likelihood.probability_of_learned_prior(target_data)
+        compare_prob = compare_likelihood.probability_of_learned_prior(target_data)
+
+        assert target_prob > compare_prob, (
+            f"{target_likelihood} likelihood should have higher probability "
+            f"for {target_likelihood} data than {compare_likelihood} likelihood"
+        )
