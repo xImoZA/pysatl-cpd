29c072cc801c785b1ae14babb5f9c99546a044b5
Alexey <alexdtat@gmail.com>
2025-04-18 21:51:59 +0300

    tests(detectors): add tests for drop detector and generalize detectors tests

--- tests/test_core/test_algorithms/test_algorithms_utils/bayesian/test_detectors_and_localizers.py
+++ tests/test_core/test_algorithms/test_algorithms_utils/bayesian/test_detectors_and_localizers.py
@@ -1,24 +1,59 @@
 import numpy as np
+import pytest
 
+from pysatl_cpd.core.algorithms.bayesian.detectors.drop import DropDetector
 from pysatl_cpd.core.algorithms.bayesian.detectors.threshold import ThresholdDetector
 from pysatl_cpd.core.algorithms.bayesian.localizers.argmax import ArgmaxLocalizer
 
 
-class TestThresholdDetector:
-    def test_detection(self):
-        run_lengths = np.full(100, 0.01)
-        detector = ThresholdDetector(threshold=0.04)
-        assert detector.detect(run_lengths), "Change point should be detected"
+@pytest.fixture(
+    params=[pytest.param((ThresholdDetector, 0.8), id="Threshold"), pytest.param((DropDetector, 0.1), id="Drop")],
+    scope="function",
+)
+def detector(request):
+    cls, threshold = request.param
+    return cls(threshold)
 
-    def test_clear(self):
-        run_lengths = np.full(100, 0.01)
-        detector = ThresholdDetector(threshold=0.04)
 
-        result1 = detector.detect(run_lengths)
+def generate_test_data(has_cp=True):
+    run_length_probs = np.full(100, 1.0)
+    run_length_probs[-1] = 50 if has_cp else 500
+
+    return run_length_probs / run_length_probs.sum()
+
+
+class TestDetectors:
+    def test_detection(self, detector):
+        before_cp = generate_test_data(has_cp=False)
+        after_cp = generate_test_data(has_cp=True)
+        print(before_cp[-1], after_cp[-1])
+        assert not detector.detect(before_cp), (
+            "Detector should not react in case of stable high probability of max run length"
+        )
+        assert detector.detect(after_cp), (
+            "Detector should react in case of significant abrupt drop of probability of max run length"
+        )
+
+    def test_clear(self, detector):
+        cp_data = generate_test_data(has_cp=True)
+
+        first_result = detector.detect(cp_data)
         detector.clear()
-        result2 = detector.detect(run_lengths)
 
-        assert result1 == result2, f"Results differ after clear: {result1} vs {result2}"
+        second_result = detector.detect(cp_data)
+
+        assert first_result == second_result, "A state was not cleared correctly"
+
+
+class TestDropDetectorSpecific:
+    def test_gradual_change(self):
+        detector = DropDetector(0.1)
+        run_lengths = np.full(100, 1.0)
+
+        for value in range(500, 490, -1):
+            run_lengths[-1] = value
+            data = run_lengths / run_lengths.sum()
+            assert not detector.detect(data), "Drop detector should not react on a gradual probability decrease"
 
 
 class TestArgmaxLocalizer:
